7891c5756c17b58e1fa5e04ed0c2f05ebf8a4d2e
Toan Thang <letoanthang204@gmail.com>
2025-06-24 18:45:36 +0700

    thang cap nhat

--- CV_AI/CV_AI/obj/Debug/net8.0/CV_AI.AssemblyInfo.cs
+++ CV_AI/CV_AI/obj/Debug/net8.0/CV_AI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CV_AI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4acf2b692933b9731e52acfe6eac427020df2f91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15291734ebf4f588afd6f896bf79a3d7edc17742")]
 [assembly: System.Reflection.AssemblyProductAttribute("CV_AI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CV_AI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
